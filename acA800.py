2efd94e52ef3e5e5f682183f861d15f357644c2b
Fernando Santoro <fsantoro@psfc.mit.edu>
2023-11-08 10:55:23 -0500

    Add SyncFreeRun capabilities to driver

--- acA800.py
+++ acA800.py
@@ -48,6 +48,15 @@ class ACA800(MDSplus.Device):
             # },
         },
         {
+            'path': ':ACQ_MODE',
+            'type': 'text',
+            'value': 'CONTINUOUS',
+            'options': ('no_write_shot',),
+            # 'ext_options': {
+            #     'tooltip': 'Basler Acquisition Mode',
+            # },
+        },
+        {
             'path': ':FPS',
             'type': 'numeric',
             'value': 5,
@@ -244,7 +253,13 @@ class ACA800(MDSplus.Device):
                 self.cam.Open()
                 
                 self.device._log_info(f"Using device {self.cam.GetDeviceInfo().GetModelName()}")
-                      
+                
+                #Enable PTP for this camera
+                self.cam.GevIEEE1588 = True
+                
+                #Acquisition Mode:
+                self.cam.AcquisitionMode = self.device.ACQ_MODE.data()
+                    
                 #Set the FPS
                 self.cam.AcquisitionFrameRateEnable.SetValue(True)
                 self.cam.AcquisitionFrameRateAbs.SetValue(float(self.device.FPS.data()))
@@ -252,30 +267,24 @@ class ACA800(MDSplus.Device):
                 self.cam.MaxNumBuffer = 1900
                 self.cam.OutputQueueSize = 1900
 
-                # Software Triggering
-                # Image acquisition starts whenever a software command is received: self.cam.ExecuteSoftwareTrigger()
-                self.cam.TriggerSelector.SetValue('FrameStart')
-                #self.cam.TriggerSelector.SetValue('FrameBurstStart')
-                self.cam.TriggerSource.SetValue('Software')
-                self.cam.TriggerMode.SetValue('On')
-                
-                # Triggering a Series of Images
-                # Whenever a rising signal on physical input line 1 is received, the camera automatically acquires a complete series of 3 images.
-                # self.cam.AcquisitionBurstFrameCount = 3
-                # self.cam.TriggerActivation.SetValue('RisingEdge')
-                # self.cam.TriggerSource.SetValue('Line1')
-
                 #self.writer = self.device.StreamWriter(self)
                 #self.writer.setDaemon(True)
                 #self.writer.start()
-
-                self.device._log_info(f"Recording {self.time_to_record} second video at {self.device.FPS.data()} fps. Max #Images = {self.frames_to_grab}")
-                                      
-                #self.cam.StartGrabbingMax(self.frames_to_grab, pylon.GrabStrategy_OneByOne)
-                self.cam.StartGrabbing(pylon.GrabStrategy_UpcomingImage)
-                #self.cam.StartGrabbing()
-
                 
+                #Triggering using SyncFreeRun timer: 
+                # https://docs.baslerweb.com/synchronous-free-run#converting-the-64-bit-timestamp-to-start-time-high-and-start-time-low
+
+                actionTime = int(self.device.TRIGGER.IN_MESSAGE.data())
+                self.cam.SyncFreeRunTimerStartTimeLow = (actionTime & 0x00000000FFFFFFFF)
+                self.cam.SyncFreeRunTimerStartTimeHigh = (actionTime & 0xFFFFFFFF00000000) >> 32
+                
+                self.cam.SyncFreeRunTimerUpdate()
+                self.cam.SyncFreeRunTimerEnable = True
+
+                self.device._log_info(f"At {actionTime} (from incoming message), recording {self.time_to_record} second video at {self.device.FPS.data()} fps. Max #Images = {self.frames_to_grab}")
+
+                self.cam.StartGrabbing()
+
                 last_frame_id = 0
                 frame_index = 0
                 while self.device.RUNNING.on and frame_index < self.frames_to_grab:
