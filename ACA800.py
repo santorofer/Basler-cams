86bffcd2e52a4845df0e154c3db572ae1cf9d089
Josh Stillerman <jas@psfc.mit.edu>
2024-01-11 10:59:47 -0500

    move thread code back into main file

--- ACA800.py
+++ ACA800.py
@@ -480,10 +480,28 @@ class ACA800(MDSplus.Device):
 #        print("thread done")
 
     def fork_stream(self):
-        from fork_stream_thread import fork_stream_thread
+        def fork_stream_thread(tree, shot, path, timeout):
+            from MDSplus import Tree
+            import os
+            import subprocess
+            print("fork_stream_thread")
+            script = f"{os.path.realpath(os.path.dirname(__file__))}/stream-basler.sh"
+            proc = subprocess.run([script, tree, str(shot), path], capture_output=True, text=True, timeout=timeout)
+            t = Tree(tree,shot)
+            d = t.getNode(path)
+            d.STDOUT.record = proc.stdout
+            d.STDERR.record = proc.stderr
+            if proc.returncode != 0:
+                raise Exception(f"Failed to fork stream: {proc.stderr}")
+            print("thread done")
+
+
+#        from fork_stream_thread import fork_stream_thread
         import threading
         thread = threading.Thread(target=fork_stream_thread, args=(self.tree.name, self.tree.shot, self.path, float(self.TIMEOUT.data())))
+        thread.setDaemon(True)
         thread.start()
+        self.PID.record = thread.native_id
     FORK_STREAM = fork_stream
 
     def stop_stream(self):
