1b0e1bc6d864ec423fead976128a0d92e8eb5654
Fernando Santoro <fsantoro@psfc.mit.edu>
2024-05-24 10:24:10 -0400

    Add some extra scripts

---
+++ _env.sh
@@ -0,0 +1,6 @@
+export DEBUG_DEVICES=4
+export PYTHONPATH=$PWD:$PYTHONPATH
+export MDS_PYDEVICE_PATH=$PWD     
+export basler_path=/home/fsantoro/shared/basler-trees/models
+export LD_LIBRARY_PATH=/home/fsantoro/shared/wrtdListen:/usr/local/lib
+
---
+++ run_a.tcl
@@ -0,0 +1,6 @@
+set tree basler
+set current basler /inc
+create pulse 0
+set tree basler /shot=0
+do /meth basler_a start_stream
+
